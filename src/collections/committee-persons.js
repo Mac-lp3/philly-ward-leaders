50f76ab13445d48650d9cc38fdc01c61bea622a3
Mac-lp3 <staticlp3@gmail.com>
2016-11-27 13:40:44 -0500

    no longer uses tabletop as the query building is not functioning correctly. Uses a URL method as before

--- src/collections/committee-persons.js
+++ src/collections/committee-persons.js
@@ -5,43 +5,44 @@ var Backbone = require('backbone'),
 require('backbone.tabletopSync');
 
 module.exports = Backbone.Collection.extend({
-     model: CommitteePerson,
-     initialize: function(models, options) {
-          
-          var params = {};
-          if(options.ward) {
-              this.ward = options.ward;
-              params.ward = ('00' + this.ward).slice(-2); // pad left
-          }
-          if(options.party){
-              this.party = options.party;
-              params.party = this.partyMap[this.party] || '';
-          } 
-
-          var url = 'https://docs.google.com/spreadsheets/d/1nkkFQUaxcGa0oDPWl1Vr1cknToIq8FxfJ7CILQrFgBM/pubhtml';
-          var storage = Tabletop.init({
-              key: url,
-              wait: true
-          });
-
-          this.tabletop = { instance: storage, sheet: 'Committeepeople' }
-     },
-     partyMap: {
+    model: CommitteePerson,
+    initialize: function(models, options) {
+       if(options.ward) this.ward = options.ward;
+       if(options.party) this.party = options.party;
+    },
+    partyMap: {
        D: 'Democrat',
        R: 'Republican'
      },
-     sync: Backbone.tabletopSync,
+    url: function() {
+
+        var baseUrl = 'https://docs.google.com/spreadsheets/d/1nkkFQUaxcGa0oDPWl1Vr1cknToIq8FxfJ7CILQrFgBM/gviz/tq?tq=';
+        var urlPostFix = '&gid=1903727742'; // selects the "Committeepeople" sheet
+        var queryString = '';
+        var params = {};
+
+        // Built query string based on provided params
+        if (this.ward) {
+
+            params.ward = ('00' + this.ward).slice(-2); // pad left
+            queryString = queryString.concat('Select * where B = ', params.ward);
+
+            if (this.party) {
+
+                params.party = this.partyMap[this.party] || '';
+                queryString = queryString.concat(' and H = "', params.party, '"');
+
+            }
+
+        } else if (this.party){
+
+            params.party = this.partyMap[this.party] || '';
+            queryString = queryString.concat('Select * where H = "', params.party, '"');
+        } 
+
+        // encode the query and build the url
+        return baseUrl.concat(encodeURIComponent(queryString), urlPostFix);
+
+     },
      comparator: function(row) { return +row.get('division'); }
 });
-
-
-/*
-,
-     url: function() {
-          var url = 'https://data.phila.gov/resource/hezk-qta3.json',
-            params = {};
-          if(this.ward) params.ward = ('00' + this.ward).slice(-2); // pad left
-          if(this.party) params.party = this.partyMap[this.party] || '';
-          return url + '?' + $.param(params);
-     }
-     */
