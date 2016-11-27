69172ed8694ada490f3bf98ce270f3aa9879fdc5
Mac-lp3 <staticlp3@gmail.com>
2016-11-27 14:22:20 -0500

    fixed typo where District was set as Division

--- src/models/committee-person.js
+++ src/models/committee-person.js
@@ -3,7 +3,7 @@ var Backbone = require('backbone'),
 
 module.exports = Backbone.Model.extend({
      initialize: function() {
-          this.set('division', this.get('District'));
+          this.set('district', this.get('District'));
           this.set('nameLowerCase', this.get('Name').toLowerCase());
           this.set('divisionOrdinal', util.getOrdinal(this.get('Division')));
      }
